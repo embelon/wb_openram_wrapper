1fae5deb18917ab3ead9d5e5577b4293bb7afd46
embelon <78412338+embelon@users.noreply.github.com>
2021-10-14 20:01:29 +0200

    Making DIE_AREA taller and thinner (40 x 300 um).

--- sky130A_sky130_fd_sc_hd_config.tcl
+++ sky130A_sky130_fd_sc_hd_config.tcl
@@ -3,5 +3,5 @@ set ::env(CLOCK_PERIOD) "10.0"
 set ::env(SYNTH_MAX_FANOUT) 5
 
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 70 90"
-set ::env(PL_TARGET_DENSITY) "0.25"
+set ::env(DIE_AREA) "0 0 40 300"
+set ::env(PL_TARGET_DENSITY) "0.2"
