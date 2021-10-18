cb73fb6a5aeb001df6fa89c73741769bf454f35d
embelon <78412338+embelon@users.noreply.github.com>
2021-10-18 20:46:23 +0200

    Removed additional power (supply) domains from config and src.

--- src/wb_openram_wrapper.v
+++ src/wb_openram_wrapper.v
@@ -22,14 +22,8 @@ module wb_openram_wrapper
 )
 (
 `ifdef USE_POWER_PINS
-    inout vdda1,	// User area 1 3.3V supply
-    inout vdda2,	// User area 2 3.3V supply
-    inout vssa1,	// User area 1 analog ground
-    inout vssa2,	// User area 2 analog ground
     inout vccd1,	// User area 1 1.8V supply
-    inout vccd2,	// User area 2 1.8v supply
     inout vssd1,	// User area 1 digital ground
-    inout vssd2,	// User area 2 digital ground
 `endif
 
     // Wishbone port A
