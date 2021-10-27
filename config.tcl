69f033d819256bd0ec48b8e8160b5e7b5a6b3145
embelon <78412338+embelon@users.noreply.github.com>
2021-10-27 12:27:44 +0200

    Added pin ordering to avoid mixing pins/signals from different interfaces together when hardening.

--- config.tcl
+++ config.tcl
@@ -37,7 +37,7 @@ set ::env(GND_NETS) [list {vssd1}]
 # set ::env(MACRO_PLACEMENT_CFG) $::env(DESIGN_DIR)/macro_placement.cfg
 
 # regular pin order seems to help with aggregating all the macros for the group project
-# set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
+set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
 
 # turn off CVC as we have multiple power domains
 set ::env(RUN_CVC) 0
