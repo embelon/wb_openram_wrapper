8b38ca95e65d04ded80fe0a32643a3dea318039f
embelon <78412338+embelon@users.noreply.github.com>
2021-12-14 22:39:53 +0100

    Adjusted Die Area and density for hardening.

--- sky130A_sky130_fd_sc_hd_config.tcl
+++ sky130A_sky130_fd_sc_hd_config.tcl
@@ -3,5 +3,5 @@ set ::env(CLOCK_PERIOD) "10.0"
 set ::env(SYNTH_MAX_FANOUT) 5
 
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 40 300"
-set ::env(PL_TARGET_DENSITY) "0.2"
+set ::env(DIE_AREA) "0 0 50 400"
+set ::env(PL_TARGET_DENSITY) "0.35"
